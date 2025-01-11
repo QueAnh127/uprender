ce448b05377375c5eace67580f2d6ce19fa8f92c
ruby on rails <maiqueanh9438@gmail.com>
2025-01-11 08:49:22 +0700

    uprenderagain

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   root "products#index"
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -13,4 +15,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+
 end
