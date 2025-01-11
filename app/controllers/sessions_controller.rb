ade1b49e8dfd510689c3b358e31ba61799f8e0af
ruby on rails <maiqueanh9438@gmail.com>
2025-01-11 09:29:18 +0700

    uprenderbacknext

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
+    unless User.exists?(email_address: "queanh94@gmail.com")
+    User.create!(
+      email_address: "queanh94@gmail.com",
+      password: "render127",
+      password_confirmation: "render127"
+      )
+    end
+      
   end
 
   def create
